455ee49c84a7474594892c80185f7e8a5a4a46f8
Matt Dillon <mddillon47@yahoo.com>
2021-02-08 21:00:21 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const section = document.getElementById('container');
+const section2 = document.querySelector('section');
+const second = document.getElementsByClassName('second');
+const third = document.getElementsByClassName('third');
+    third[1];
+const section3 = document.getElementById('container');
+    section3.innerText = 'Hello!';
+const divs = document.querySelectorAll('div');
+    const secondDiv = divs[1];
+    secondDiv.className += ' main';
+secondDiv.classList.remove('main');
+const li = document.createElement('li');
+li.innerText = "four";
+const ul = document.querySelector('ul');
+    ul.append(li);
+const ol = document.querySelector('ol');
+const olChildren = ol.children
+for (let child of olChildren) {
+    child.style.backgroundColor = 'green';
+}
+const div = document.querySelectorAll('div')
+    const divFooter = div[1];
+    divFooter.classList.remove('footer')
+
+
